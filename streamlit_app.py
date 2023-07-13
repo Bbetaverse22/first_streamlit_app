34ff85511b064bdd8a86fdc080ef7708f2183678
Betul <47547579+Bbetaverse22@users.noreply.github.com>
2023-07-13 14:12:05 -0500

    Update streamlit_app.py
    corrected the syntax

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@
 import streamlit
 streamlit.title('A Study Guide for Testers')
 streamlit.header('How to improve your Skills')
-streamlit.text('For a tester it's very important to stay upto date with recent technology on testing and overall on software field')
+streamlit.text('For a tester it is very important to stay upto date with recent technology on testing and overall on software field')
 streamlit.text('This is because a tester should have different hats, needs to be wear when there is development, when there is testing, or dev-ops or databases,..')
