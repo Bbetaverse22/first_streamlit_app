6f0f2834d67170c42d01963ea036a85d0e8d380b
Betul <47547579+Bbetaverse22@users.noreply.github.com>
2023-07-13 14:31:34 -0500

    Update streamlit_app.py
    added the fruitList

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-
+import pandas
 import streamlit
 streamlit.title('A Study Guide for Testers')
 streamlit.header('How to improve your Skills')
@@ -14,3 +14,5 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
