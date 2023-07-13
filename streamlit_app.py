13941b51de24686385f3e4ce70f83bec4df3e07e
Betul <47547579+Bbetaverse22@users.noreply.github.com>
2023-07-13 14:14:45 -0500

    Update streamlit_app.py
    added more texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,8 @@
 import streamlit
 streamlit.title('A Study Guide for Testers')
 streamlit.header('How to improve your Skills')
-streamlit.text('For a tester it is very important to stay upto date with recent technology on testing and overall on software field')
-streamlit.text('This is because a tester should have different hats, needs to be wear when there is development, when there is testing, or dev-ops or databases,..')
+streamlit.text('For a tester it is very important to stay upto date with the recent')
+streamlit.text('technology on testing and overall on software field')
+streamlit.text('This is because a tester should have different hats')
+streamlit.text('It needs to be used as a deveoper or a tester or a dev-ops or')
+streamlit.text('an IT staff or a bussiness analyst or maybe a project manager etc.')
